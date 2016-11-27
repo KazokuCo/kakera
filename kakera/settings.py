e2f5da14402d32ca036756d9d357f16485a935a0
uppfinnarn <uppfinnarn@gmail.com>
2016-11-27 22:47:39 +0100

    /public/media and /public/static for static stuff

--- kakera/settings.py
+++ kakera/settings.py
@@ -140,11 +140,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
 
 
 # Wagtail
