8ce98a740deb5bf9d88fc23c840e1317878b1534
uppfinnarn <uppfinnarn@gmail.com>
2016-11-29 06:26:32 +0100

    Reindent

--- kakera_blog/models.py
+++ kakera_blog/models.py
@@ -10,58 +10,58 @@ from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailembeds.blocks import EmbedBlock
 
 class MarkdownBlock(TextBlock):
-	class Meta:
-		template = 'kakera_blog/blocks/markdown.html'
+    class Meta:
+        template = 'kakera_blog/blocks/markdown.html'
 
 class BlogPage(Page):
-	published = models.DateTimeField()
-	
-	cover_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
-	cover_embed = models.CharField(max_length=1000, null=True, blank=True)
-	
-	body = StreamField([
-		('markdown', MarkdownBlock()),
-		('image', ImageChooserBlock()),
-		('embed', EmbedBlock()),
-	])
-	
-	search_fields = Page.search_fields + [
-		index.SearchField('title'),
-	]
-	
-	content_panels = Page.content_panels + [
-		FieldPanel('published'),
-		MultiFieldPanel([
-			ImageChooserPanel('cover_image'),
-			FieldPanel('cover_embed'),
-		], "Cover"),
-		StreamFieldPanel('body'),
-	]
-	
-	# Only allow blog posts under index pages, disallow subpages
-	parent_page_types = ['kakera_blog.BlogIndexPage']
-	subpage_types = []
+    published = models.DateTimeField()
+
+    cover_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+    cover_embed = models.CharField(max_length=1000, null=True, blank=True)
+
+    body = StreamField([
+        ('markdown', MarkdownBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ])
+
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('published'),
+        MultiFieldPanel([
+            ImageChooserPanel('cover_image'),
+            FieldPanel('cover_embed'),
+        ], "Cover"),
+        StreamFieldPanel('body'),
+    ]
+
+    # Only allow blog posts under index pages, disallow subpages
+    parent_page_types = ['kakera_blog.BlogIndexPage']
+    subpage_types = []
 
 class BlogIndexPage(Page):
-	def get_context(self, request):
-		context = super(BlogIndexPage, self).get_context(request)
-		context['blog_entries'] = BlogPage.objects.child_of(self).live()
-		return context
+    def get_context(self, request):
+        context = super(BlogIndexPage, self).get_context(request)
+        context['blog_entries'] = BlogPage.objects.child_of(self).live()
+        return context
 
 class StaticPage(Page):
-	cover_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
-	
-	body = StreamField([
-		('markdown', MarkdownBlock()),
-		('image', ImageChooserBlock()),
-		('embed', EmbedBlock()),
-	])
-	
-	search_fields = Page.search_fields + [
-		index.SearchField('title'),
-	]
-	
-	content_panels = Page.content_panels + [
-		ImageChooserPanel('cover_image'),
-		StreamFieldPanel('body'),
-	]
+    cover_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+
+    body = StreamField([
+        ('markdown', MarkdownBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ])
+
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+    ]
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('cover_image'),
+        StreamFieldPanel('body'),
+    ]
--- kakera_core/models.py
+++ kakera_core/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-	bio = models.TextField(blank=True)
-	twitter = models.CharField(max_length=15, blank=True)
+    bio = models.TextField(blank=True)
+    twitter = models.CharField(max_length=15, blank=True)
