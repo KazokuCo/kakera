fce976e21a3897203e1107ad8d0b150bd98e685e
uppfinnarn <uppfinnarn@gmail.com>
2016-12-15 00:23:00 +0100

    import_ghost_users management command

---
+++ kakera_core/management/commands/import_ghost_users.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from kakera_core.models import User
+
+class Command(BaseCommand):
+    help = "Imports users from a Ghost database dump"
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=argparse.FileType('r', encoding='utf-8'), help='JSON file to load.')
+
+    def handle(self, *args, **options):
+        data = json.load(options['file'])
+
+        with transaction.atomic():
+            for userdata in data['db'][0]['data']['users']:
+                # self.stdout.write(json.dumps(userdata))
+
+                username = userdata['name']
+                email = userdata['email'] or ""
+                twitter = userdata['twitter'] or ""
+                bio = userdata['bio'] or ""
+
+                if twitter.startswith('@'):
+                    twitter = twitter[1:]
+
+                try:
+                    user = User.objects.get(username=username)
+                    self.stdout.write("  {0}".format(username))
+                except User.DoesNotExist:
+                    user = User.objects.create_user(username=username)
+                    self.stdout.write("+ {0}".format(username))
+
+                user.email = email
+                user.bio = bio
+                user.twitter = twitter
+                user.full_clean()
+                user.save()
