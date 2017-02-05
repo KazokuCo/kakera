63d12f386c6afbad8fc4ccbb7f6b72b73a02e739
uppfinnarn <uppfinnarn@gmail.com>
2017-02-05 19:00:29 +0100

    Load .env files

--- kakera/settings.py
+++ kakera/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Load environment variables from .env
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
