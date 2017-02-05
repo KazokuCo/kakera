fe7964634eeb90f68f0bf969f092b6dfa87b6708
uppfinnarn <uppfinnarn@gmail.com>
2017-02-05 22:01:09 +0100

    Pre-render body and excerpt

---
+++ kakera_blog/management/commands/rebake.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from kakera_blog.models import BlogPage, StaticPage
+
+ADMIN_FILTERS = [
+    'max-165x165',
+    'max-800x600',
+]
+
+COVER_FILTERS = [
+    'original',
+    'max-500x300',
+] + ADMIN_FILTERS
+
+BLOCK_FILTERS = [
+    'original',
+] + ADMIN_FILTERS
+
+class Command(BaseCommand):
+    help = "Rebuilds precompiled post HTML"
+
+    def handle(self, *args, **options):
+        for page in list(BlogPage.objects.all()) + list(StaticPage.objects.all()):
+            print("## {}".format(page.title))
+            with transaction.atomic():
+                if page.cover_image:
+                    print("-> rendering cover...")
+                    for ft in COVER_FILTERS:
+                        print("   -> {0}".format(ft))
+                        page.cover_image.get_rendition(ft)
+                for block in page.body:
+                    if block.block_type == 'image':
+                        print("-> rendering: {}".format(block.value))
+                        for ft in BLOCK_FILTERS:
+                            print("    -> {0}".format(ft))
+                            block.value.get_rendition(ft)
+                print("-> cooking...")
+                page.save()
