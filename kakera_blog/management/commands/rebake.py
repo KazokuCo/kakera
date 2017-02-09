ccba7749df1597f30740307a9173a62209605230
uppfinnarn <uppfinnarn@gmail.com>
2017-02-09 21:41:47 +0100

    ./manage.py rebake --quick skips image rendering

--- kakera_blog/management/commands/rebake.py
+++ kakera_blog/management/commands/rebake.py
@@ -19,20 +19,24 @@ BLOCK_FILTERS = [
 class Command(BaseCommand):
     help = "Rebuilds precompiled post HTML"
 
+    def add_arguments(self, parser):
+        parser.add_argument('--quick', action='store_true')
+
     def handle(self, *args, **options):
         for page in list(BlogPage.objects.all()) + list(StaticPage.objects.all()):
             print("## {}".format(page.title))
             with transaction.atomic():
-                if page.cover_image:
-                    print("-> rendering cover...")
-                    for ft in COVER_FILTERS:
-                        print("   -> {0}".format(ft))
-                        page.cover_image.get_rendition(ft)
-                for block in page.body:
-                    if block.block_type == 'image':
-                        print("-> rendering: {}".format(block.value))
-                        for ft in BLOCK_FILTERS:
-                            print("    -> {0}".format(ft))
-                            block.value.get_rendition(ft)
+                if not options['quick']:
+                    if page.cover_image:
+                        print("-> rendering cover...")
+                        for ft in COVER_FILTERS:
+                            print("   -> {0}".format(ft))
+                            page.cover_image.get_rendition(ft)
+                    for block in page.body:
+                        if block.block_type == 'image':
+                            print("-> rendering: {}".format(block.value))
+                            for ft in BLOCK_FILTERS:
+                                print("    -> {0}".format(ft))
+                                block.value.get_rendition(ft)
                 print("-> cooking...")
                 page.save()
